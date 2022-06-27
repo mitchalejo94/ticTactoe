0d91584a1e0b730834c6744037687e9142b5a1fd
Mitchel Alejo <michalejo94@gmail.com>
2022-06-26 19:09:42 -0500

    note

---
+++ app.js
@@ -0,0 +1,18 @@
+const gameState = {
+    players: ["x", "o"],
+    board: [
+        [null, null, null]
+        [null, null, null]
+        [null, null, null]
+    ],
+    currentPlayer: "x",
+
+};
+
+//HTML board
+board.addEventListener ("click", ("event") => {
+   
+    }
+
+
+)
